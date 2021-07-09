4518698baf2621f1a72e828419f6e41c4fd6befb
mafgit <abdskinny@gmail.com>
2021-07-10 01:19:28 +0500

    gonna merge with master

--- src/flag_options.ts
+++ src/flag_options.ts
@@ -3,15 +3,18 @@ export type flag_options = {
   long: string
   will_have_value?: boolean
   default_value?: any
+  on_value?: (
+    value: string,
+    cb: (err: string | null, new_value?: any) => any
+  ) => void
   // Validations:
   required?: boolean
   must_not_be_empty?: boolean
   enum?: Array<string>
   min?: number
   max?: number
-  minLength?: number // done
-  maxLength?: number // done
-  //
+  minLength?: number
+  maxLength?: number
   type?:
     | 'integer'
     | 'float'
@@ -19,8 +22,4 @@ export type flag_options = {
     | 'arr_of_integer'
     | 'arr_of_float'
     | 'arr_of_string'
-  on_value?: (
-    value: string,
-    cb: (err: string | null, new_value?: any) => any
-  ) => void
 }
